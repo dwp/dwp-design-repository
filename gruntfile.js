1039c798601a9447e55cb89c56693c83445c16ec
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-05-18 19:56:25 +0100

    Added grunt nodemon and grunt-watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function (grunt) {
-  // Project configuration.
   grunt.initConfig({
     sass: {
       dev: {
@@ -15,10 +14,51 @@ module.exports = function (grunt) {
           ext: '.css'
         }]
       }
+    },
+
+    watch: {
+      jsFiles: {
+        files: ['app/assets/javascripts/**/*.js'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      },
+
+      sassFiles: {
+        files: ['app/assets/sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'bin/www',
+        options: {
+          ext: 'js, json',
+          ignore: ['node_modules/**', 'app/assets/**', 'public/**'],
+          args: grunt.option.flags()
+        }
+      }
+    },
+
+    concurrent: {
+      target: {
+        tasks: ['watch', 'nodemon'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'concurrent:target']);
 };
