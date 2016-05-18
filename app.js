0cc0f394294a1f670207fb353f8dcf31a060b7f8
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-05-18 17:40:26 +0100

    Set up layout and nunjucks

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const nunjucks = require('express-nunjucks');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,25 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+
+// Configuring the template system.
+nunjucks.setup({
+    // (default: true) controls if output with dangerous characters are escaped automatically.
+    autoescape: true,
+    // (default: false) throw errors when outputting a null/undefined value.
+    throwOnUndefined: false,
+    // (default: false) automatically remove trailing newlines from a block/tag.
+    trimBlocks: false,
+    // (default: false) automatically remove leading whitespace from a block/tag.
+    lstripBlocks: false,
+    // (default: false) if true, the system will automatically update templates when they are changed on the filesystem.
+    watch: true,
+    // (default: false) if true, the system will avoid using a cache and templates will be recompiled every single time.
+    noCache: true,
+    // (default: see nunjucks syntax) defines the syntax for nunjucks tags.
+    tags: {}
+}, app);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
