9f89546830c22807ce0d93cbfebf805fb4ba3f5c
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-05-26 11:06:31 +0100

    Amended start for gruntfile

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const gruntfile = path.join(__dirname, '/Gruntfile.js');
+const gruntfile = path.join(__dirname, 'gruntfile.js');
 
 require(path.join(__dirname, '/node_modules/grunt/lib/grunt.js')).cli({
   gruntfile
